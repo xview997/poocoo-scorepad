53cc717e1cb2dc19e4bc23bf38d83e79b73b1a6c
Tomasz Jawroski <tomasz.jaworski@ex7.pl>
2020-05-17 09:04:16 +0200

    Updates

---
+++ precache-manifest.534aeb57b9da49460e577a3689f752f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaa7d0001ed7e32f0f1706182795af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20865087b593763532fb",
+    "url": "/static/css/main.0ab971ab.chunk.css"
+  },
+  {
+    "revision": "676ae8fe61b12bd9f2f9",
+    "url": "/static/js/2.d6d00cab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6d00cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20865087b593763532fb",
+    "url": "/static/js/main.f7aee254.chunk.js"
+  },
+  {
+    "revision": "7e9355609c7816654691",
+    "url": "/static/js/runtime-main.4fa149b0.js"
+  }
+]);
